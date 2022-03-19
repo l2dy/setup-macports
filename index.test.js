f6b07757c5b31324ee7ef1771766b3771a652832
Zero King <l2dy@icloud.com>
2022-03-19 08:54:27 +0000

    Add getLatestVersion function

--- index.test.js
+++ index.test.js
@@ -5,9 +5,14 @@ const path = require('path');
 
 const MACPORTS_VERSION = '2.7.2';
 
-test('returns correct .pkg URL', () => {
+test('generates pkg URL', () => {
   const url = mputil.getPkgURL('Darwin Kernel Version 19.6.0', MACPORTS_VERSION);
   expect(url).toEqual('https://github.com/macports/macports-base/releases/download/v2.7.2/MacPorts-2.7.2-10.15-Catalina.pkg');
+})
+
+test('fetch latest version', async () => {
+  const version = await mputil.getLatestVersion();
+  expect(/^\d/.exec(version)).not.toBeNull();
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
--- mputil.js
+++ mputil.js
@@ -1,15 +1,40 @@
+const httpm = require('@actions/http-client');
+
 const osMap = {
   "19": "10.15-Catalina",
   "20": "11-BigSur",
 }
 
-let getPkgURL = function (osVersion, pkgVersion) {
+function regexpCapture(regexp, s) {
+  let match = regexp.exec(s);
+  if (match === null) {
+    return ""
+  }
+  return match[1];
+}
+
+let getLatestVersion = async function() {
+  const versionRE = /v([0-9.]+)$/;
+  const RELEASE_URL = 'https://raw.githubusercontent.com/macports/macports-base/master/config/RELEASE_URL';
+  const http = new httpm.HttpClient();
+  let res = await http.get(RELEASE_URL);
+  if (res.message.statusCode !== 200) {
+    throw new Error(`fetch RELEASE_URL failed, status ${res.message.statusCode}`);
+  }
+  let body = await res.readBody();
+  let version = regexpCapture(versionRE, body.trim());
+  if (version === "") {
+    throw new Error(`RELEASE_URL match failed: ${body}`);
+  }
+  return version;
+}
+
+let getPkgURL = function(osVersion, pkgVersion) {
   const osMajorRE = /^Darwin Kernel Version (\d+)\./;
-  const osMatch = osMajorRE.exec(osVersion);
-  if (osMatch === null) {
+  const osMajor = regexpCapture(osMajorRE, osVersion);
+  if (osMajor === "") {
     throw new Error(`unrecognized OS version: ${osVersion}`);
   }
-  const osMajor = osMatch[1];
   const osStr = osMap[osMajor];
   if (osStr === undefined) {
     throw new Error(`unsupported OS version: ${osVersion}`);
@@ -17,4 +42,4 @@ let getPkgURL = function (osVersion, pkgVersion) {
   return `https://github.com/macports/macports-base/releases/download/v${pkgVersion}/MacPorts-${pkgVersion}-${osStr}.pkg`;
 }
 
-module.exports.getPkgURL = getPkgURL;
+module.exports = { getPkgURL, getLatestVersion };
